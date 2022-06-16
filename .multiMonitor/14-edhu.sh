e94d008039a8247123a381c6ddf2623e78c51f70
dongyi-guo <dguo0006@uni.sydney.edu.au>
2022-06-17 02:38:47 +0800

    Fix Wallpapers

--- .multiMonitor/14-edhu.sh
+++ .multiMonitor/14-edhu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1366x768 --pos 277x1080 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output VGA-1 --off
-feh --bg-scale /home/dongyi-guo/.Wallpaper/archlinux.png
+xrandr --output eDP-1 --mode 1366x768 --pos 277x1080 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VGA-1 --off
+feh --bg-scale $HOME/.Wallpaper/wallpaper.png
